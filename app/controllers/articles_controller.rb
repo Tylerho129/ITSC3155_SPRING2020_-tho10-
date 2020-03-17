40ed6eb2832ac854035b2b7966c6ad5c2f35ffd5
tho10 <tho10@uncc.edu>
2020-03-17 01:23:21 +0000

    List all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
+    def index
+        @articles= Article.all
+    end
     def new 
     end
     
